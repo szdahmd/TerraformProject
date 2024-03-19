3fab78515ac51a6a42990daf69ee219fe129d986
szdahmd <163073637+szdahmd@users.noreply.github.com>
2024-03-18 22:03:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "RHEL-Server" {
-  count         = 1
+  count         = 5
   ami           = "ami-0fe630eb857a6ec83"
   instance_type = "t2.micro"
   key_name      = "SazidAkeypair"
   tags = {
-    Name = "RHEL-Two"
+    Name = "RHEL-Machines"
   }
   root_block_device {
-    volume_size = 12 
+    volume_size = 18 
     volume_type = "gp3"
     encrypted   = true
   }
